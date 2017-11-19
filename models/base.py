702ffca8fdb855bceccf1fdc4244dce36f4725a9
juancarlospaco <juancarlospaco@gmail.com>
2017-11-19 00:46:41 -0300

    Add base model.

---
+++ models/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""BaseModel for all models, with a PooledPostgresqlExtDatabase connection."""
+
+
+from datetime import datetime
+from uuid import uuid4
+
+from peewee import DateTimeField, UUIDField
+
+from apistar_peewee import get_model_base
+
+
+Model = get_model_base()
+
+
+class BaseModel(Model):
+    uuid = UUIDField(default=uuid4)                     # Each with a UUID4.
+    timestamp = DateTimeField(default=datetime.utcnow)  # Each with timestamp.
+
+    def __repr__(self):
+        try:  # Try to get all the fields on the model for the str repr.
+            all_the_fields = self._meta.fields.keys()
+        except Exception:
+            all_the_fields = ""
+        return (f'{self.__class__.__name__}(uuid={self.uuid}, '
+                f'timestamp={self.timestamp}, {all_the_fields})')
